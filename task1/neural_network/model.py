b664e2a25dbd7c7d69ccb1a2833fb2a9ea2efd4c
SarangT123 <sarang.thekkedathpr@gmail.com>
2025-12-23 00:40:03 +0530

    final NN module code (hopefully)

--- task1/neural_network/model.py
+++ task1/neural_network/model.py
@@ -3,7 +3,9 @@ from math import exp
 
 
 class DataPoint:
-    pass
+    def __init__(self, inputs=None, expected_outputs=None):
+        self.inputs = inputs
+        self.expected_outputs = expected_outputs
 
 
 class Layer:
@@ -11,28 +13,26 @@ class Layer:
         self.n_inputs = n_inputs
         self.n_neurons = n_neurons
         self.weights = np.random.randn(n_inputs,n_neurons)
-        self.biases = np.zeros(1,n_neurons)
+        self.biases = np.zeros(n_neurons)
         self.inputs = None
         self.weight_gradients = np.zeros((n_inputs, n_neurons))
         self.bias_gradients = np.zeros(n_neurons)
 
-    def ReLU(self,output):
-        relu = []
-        for i in output:
-            relu.append(0 if i<=0 else i)
-        return relu
+    def ReLU(self, output):
+        return np.maximum(0, output)
 
-    def sigmoid(self,output):
-        sigmoid = []
-        for y in output:
-            sigmoid.append(1/(1+exp(-y)))
-        return sigmoid
+    def sigmoid(self, output):
+        return 1 / (1 + np.exp(-output))
     
-    def sigmoid_activation_derivative(self,output):
+
+    def sigmoid_activation_derivative(self, z):
         # y = 1/(1+e^(-x))
         # dy/dx = y(1-y)
-        y = self.sigmoid(output)
-        return y*(1-y)
+        s = self.sigmoid(z)
+        return s * (1 - s)
+
+    def ReLU_activation_derivative(self, z):
+        return 1.0 if z > 0 else 0.0
 
     
     def square_neuron_cost(self,pred,target):
@@ -47,28 +47,30 @@ class Layer:
 
 
 
-    def calculate_output(self,inputs):
-        self.inputs = inputs
-        self.output = self.sigmoid(np.dot(inputs,self.weights)+self.biases)
+    def calculate_output(self, inputs):
+        self.inputs = inputs.flatten() if inputs.ndim > 1 else inputs  # Flatten to 1D
+        z = np.dot(self.inputs, self.weights) + self.biases
+        self.output = self.sigmoid(z).flatten()  # Apply activation function and flatten
         return self.output
 
 
 
-    def calculate_output_layer_neuron_values(self,expected_outputs):
+    def calculate_output_layer_neuron_values(self, expected_outputs):
         neuron_values = []
-        length = len(expected_outputs)
-        for i in range(0,length):
-
-            # Eval PD for current node cost/activation and cost/output(z=xi*wi + bi)
-            loss_derivative = self.square_neuron_cost_derivative(self.sigmoid[i],expected_outputs[i])
-            activation_derivative_value = self.sigmoid_activation_derivative(self.sigmoid[i],expected_outputs[i])
-
-        # neuron_values  =  da/dw * dc/da
-        neuron_values.append(loss_derivative*activation_derivative_value)
-        return neuron_values
+        # Flatten output if it's 2D
+        output_flat = self.output.flatten()
+        
+        for i in range(len(expected_outputs)):
+            # Get scalar values
+            loss_derivative = self.square_neuron_cost_derivative(output_flat[i], expected_outputs[i])
+            activation_derivative_value = self.sigmoid_activation_derivative(output_flat[i])
+            neuron_values.append(loss_derivative * activation_derivative_value)
+        
+        return np.array(neuron_values)  # Convert to numpy array
+    
     
     def calculate_hidden_layer_neuron_values(self,old_layer,old_neuron_values):
-        new_neuron_values = np.zeros() #aka nnv for short
+        new_neuron_values = np.zeros(self.n_neurons)   #aka nnv for short
         nnv_length = self.n_neurons # num of neurons out will be length of nnv
         onv_length = len(old_neuron_values)
         for new_neuron_index in range(0,nnv_length):
@@ -84,7 +86,32 @@ class Layer:
         return new_neuron_values
 
 
+    def update_gradients(self, neuron_values):
+        
+        for neuron_out in range(0, self.n_neurons):
+            
+            for neuron_in in range(0, self.n_inputs):
+                dz_dw = self.inputs[neuron_in]
+                dL_dw = dz_dw * neuron_values[neuron_out]
+                self.weight_gradients[neuron_in][neuron_out] += dL_dw
+
+            dL_db = neuron_values[neuron_out]
+            self.bias_gradients[neuron_out] += dL_db
     
+
+    def apply_gradients(self, learn_rate):
+        for neuron_out in range(0, self.n_neurons):
+            self.biases[neuron_out] -= self.bias_gradients[neuron_out] * learn_rate
+            for neuron_in in range(0, self.n_inputs):
+                self.weights[neuron_in][neuron_out] -= self.weight_gradients[neuron_in][neuron_out] * learn_rate
+        
+    def clear_gradients(self):
+        # Reset all gradients to zero
+        self.weight_gradients = np.zeros((self.n_inputs, self.n_neurons))
+        self.bias_gradients = np.zeros(self.n_neurons)
+
+
+    """
     def update_gradients(self,neuron_values):
         for neuron_out in range(0,self.n_neurons):
             for neuron_in in range(0,self.n_inputs):
@@ -102,16 +129,7 @@ class Layer:
             # dL/dw = 1* neuron_values 
             dL_db =  neuron_values
             self.bias_gradients[neuron_out] += dL_db
-
-    def apply_gradients(self,learn_rate):
-        for neuron_out in range(0,self.n_neurons):
-            self.biases[neuron_out] -= self.bias_gradients[neuron_out]*learn_rate
-            for neuron_in in range(0,self.n_inputs):
-                self.weights[neuron_out][neuron_in] -= self.weight_gradients[neuron_in][neuron_out] * learn_rate
-    def clear_gradients(self):
-        # Reset all gradients to zero
-        self.weight_gradients = np.zeros((self.n_inputs, self.n_neurons))
-        self.bias_gradients = np.zeros(self.n_neurons)
+    """
 
 
 
@@ -124,13 +142,14 @@ class NeuralNetwork:
         for i in range(0,self.n_layers-1):
             self.layers.append(Layer(layer_sizes[i],layer_sizes[i+1]))
     
-    def calculate_output(self,input=None):
-        input = input if input is not None else self.layers
-        self.outputs = []
-        for layer in input:
-            self.outputs.append(layer.calculate_output())
-        return self.outputs
-    
+    def calculate_output(self, inputs):
+        """Forward pass through all layers"""
+        current_input = inputs
+        for layer in self.layers:
+            current_output = layer.calculate_output(current_input)
+            current_input = current_output
+        return current_output
+        
 
     def loss_single(self,data_point):
         # data_point is a single set of input thats it is a list of inputs the same length as the input neurons
@@ -147,24 +166,27 @@ class NeuralNetwork:
             total_cost += self.loss_single(data_point)
 
     # Reminder : document this functions working in an external file later 
-    def update_all_gradients(self,data_point):
-        # Run input through the network so that each layer will store values 
+
+
+    def update_all_gradients(self, data_point):
+        # Run input through the network
         self.calculate_output(data_point.inputs)
 
-        # update gradients of output layer
-        output_layer = self.layers[self.n_layers-1]
+        # Update gradients of output layer
+        output_layer = self.layers[-1]  # Use -1 to get last layer
         neuron_values = output_layer.calculate_output_layer_neuron_values(data_point.expected_outputs)
         output_layer.update_gradients(neuron_values)
 
-        # loop through all hidden layers and update gradients 
-        for hidden_layer_index in range(self.n_layers-2, 0,-1):
-            #update gradients of the hidden layer
+        # Loop through all hidden layers
+        for hidden_layer_index in range(len(self.layers) - 2, -1, -1):
             hidden_layer = self.layers[hidden_layer_index]
-            neuron_values = hidden_layer.calculate_hidden_layer_neuron_values(self.layers[hidden_layer_index+1],neuron_values)
-            hidden_layer.update_gradients(neuron_values) 
+            neuron_values = hidden_layer.calculate_hidden_layer_neuron_values(
+                self.layers[hidden_layer_index + 1], 
+                neuron_values
+            )
+            hidden_layer.update_gradients(neuron_values)
 
-
-    
+        
 
     def learn(self,training_batch,learn_rate):
 
@@ -175,15 +197,8 @@ class NeuralNetwork:
         for layer in self.layers:
             layer.apply_gradients(learn_rate / len(training_batch))
             layer.clear_gradients()
-    
-    
-
-    
         
-
-    def classify():
-        pass
-
+        
 
     
 
